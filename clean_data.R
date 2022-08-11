d16e934d9b231be3f7301338c2d21e39ddff0dbe
ekuyuk <ekuyuk04@gmail.com>
2022-08-10 19:47:01 -0500

    changes

---
+++ clean_data.R
@@ -0,0 +1,6 @@
+ec <- read.csv(file = "social_capital_county.csv")|> 
+  select(county, count_name, pop2018, ec_county)|> 
+  drop_na()|> 
+  mutate(state = str_remove(county_name, ".*, "))
+
+write_rds(ec, "ec.rds")
